d198fac274bcb54b1addbe500c6b0749593bbbec
Alex Kim <ajkim141@gmail.com>
2014-08-22 11:34:06 -0400

    implemented some suggested changes

--- minesweeper.py
+++ minesweeper.py
@@ -2,26 +2,18 @@
 
 import random
 
-# Initial board setup
+# Initial setup
 
-r1 = ["O","O","O","O","O","O","O"]
-r2 = ["O","O","O","O","O","O","O"]
-r3 = ["O","O","O","O","O","O","O"]
-r4 = ["O","O","O","O","O","O","O"]
-r5 = ["O","O","O","O","O","O","O"]
-r6 = ["O","O","O","O","O","O","O"]
-r7 = ["O","O","O","O","O","O","O"]
-
-game_board = [r1, r2, r3, r4, r5, r6, r7]
+game_board = list()
 game_on = True
 
 # Draw the board
 def draw_grid(board):
     print ("   1   2   3   4   5   6   7")
-    rn = 1
+    row_num = 1
     for i in board:
-        print rn,
-        rn += 1
+        print row_num,
+        row_num += 1
         interpret_row(i)
         print "\n"
 
@@ -31,17 +23,10 @@ def draw_grid(board):
 # 0: empty
 # 1: mine
 def random_mines(board):
-    board[0] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
-    board[1] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
-    board[2] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
-    board[3] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
-    board[4] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
-    board[5] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
-    board[6] = [[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]]
+    for i in xrange(7):
+        board.append([[False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)], [False, random.randint(0, 1)]])
 
-    game_board = [board[0], board[1], board[2], board[3], board[4], board[5], board[6]]
-
-    return game_board
+    return board
 
 # Handle hiding and revealing spaces
 def interpret_row(row):
